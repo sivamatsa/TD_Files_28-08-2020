f1a7842b6899ebc7e7acf28eb2ecf55700340433
sivamatsa <63741347+sivamatsa@users.noreply.github.com>
2020-08-28 17:47:02 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Git Classes - Apssdc'
+copyright = '2020, sivamatsa'
+author = 'Siva Matsa'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
